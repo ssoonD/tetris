30f15497e8316b075a39e599693e70919bba031e
ssoonD <ksj96226@gmail.com>
2020-11-12 18:02:48 +0900

    feat: 파란색 J 테트로미노 그리기

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 let board = new Board();
 
 function play() {
-    board.reset();
-    console.table(board.grid);
+    board = getEmptyBoard();
+    let piece = new Piece(ctx);
+    piece.draw();
+
+    board.piece = piece;
 }
