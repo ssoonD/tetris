c01ad7cd5f0703f4592f8ba16135ff4c94df6215
ssoonD <ksj96226@gmail.com>
2020-11-12 13:11:03 +0900

    feat: board 크기 지정

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+// 게임 설정과 규칙 정의
+
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// 게임 초기화와 종료 로직 코드
+
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+// 상수를 사용해 캔버스의 크기를 계산한다.
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// 블록의 크기를 변경한다.
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
